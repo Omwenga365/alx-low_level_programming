d4681730f44e5259e998d84a61626a020cf01a45
Omwenga365 <victoromwenga365@gmail.com>
2023-10-30 14:54:27 -0700

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,34 @@
 #include "main.h"
-#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 /**
- * append_text_to_file - appends text at the end of a file.
- * @filename: name of file
- * @text_content: string
- * Return: 1 for success || -1 for failure
+ * append_text_to_file - append text to the end of a file
+ * @filename: name of file to write into.
+ * @text_content: text to append to file
+ *
+ * Return: 1 (SUCCESS), else -1 (FAILURE)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, _write;
+	int fd, err, len;
 
+	fd = err = len = 0;
 	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
-	if (!text_content)
-	{
-		close(fd);
+	else if (!text_content || !text_content[0])
 		return (1);
-	}
-	_write = write(fd, text_content, strlen(text_content));
-	if (_write == -1 || _write != strlen(text_content))
-	{
-		close(fd);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
 		return (-1);
-	}
+	while (text_content[len])
+		len++;
+	err = write(fd, text_content, len);
+	if (err < 0)
+		return (-1);
 	close(fd);
 	return (1);
 }
